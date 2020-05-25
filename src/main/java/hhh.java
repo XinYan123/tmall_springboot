e0e0e6cb5e093ff1585c2dc23162d9dcc5fe0710
xinyan123456 <504591201@qq.com>
2020-05-25 10:20:31 +0800

    开始项目

---
+++ src/main/java/hhh.java
@@ -0,0 +1,12 @@
+/**
+ * @description:
+ * @author: Frankcrose
+ * @createDate: 2020/5/25 10:20
+ */
+public class hhh {
+
+
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+    }
+}
