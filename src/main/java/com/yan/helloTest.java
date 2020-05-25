0ce5a0acdf4dfc7271ae769a991f619cd1505940
xinyan123456 <504591201@qq.com>
2020-05-25 09:34:50 +0800

    testgit

---
+++ src/main/java/com/yan/helloTest.java
@@ -0,0 +1,12 @@
+package com.yan;
+
+/**
+ * @description:
+ * @author: Frankcrose
+ * @createDate: 2020/5/25 9:29
+ */
+public class helloTest {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
