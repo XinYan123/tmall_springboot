00ab016432ee8724b2e3d3818640af154d8a22ec
xinyan123456 <504591201@qq.com>
2020-05-26 16:29:00 +0800

    开始项目，category完成 404是由于静态资源路径错误
    完成分页功能，ListCategory.html
    的list方法中有代码跳转问题

--- src/main/java/com/yan/Service/CategoryService.java
+++ src/main/java/com/yan/Service/CategoryService.java
@@ -2,7 +2,11 @@ package com.yan.Service;
 
 import com.yan.dao.CategoryDAO;
 import com.yan.pojo.Category;
+import com.yan.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +23,16 @@ public class CategoryService {
 
     @Autowired//自动进行对象构建，注入依赖
     CategoryDAO categoryDAO;
+
+    //调用DAO层之后进行分页处理，带参数的list方法
+    public Page4Navigator<Category> list(int start, int size, int navigatePages) {
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(start, size,sort);
+        Page pageFromJPA =categoryDAO.findAll(pageable);
+
+        return new Page4Navigator<>(pageFromJPA,navigatePages);
+    }
+
 
     public List<Category> list() {
         Sort sort = new Sort(Sort.Direction.DESC, "id");
--- src/main/java/com/yan/controller/CategoryController.java
+++ src/main/java/com/yan/controller/CategoryController.java
@@ -2,8 +2,10 @@ package com.yan.controller;
 
 import com.yan.Service.CategoryService;
 import com.yan.pojo.Category;
+import com.yan.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,8 +22,19 @@ public class CategoryController {
     @Autowired//自动装配 CategoryService
     CategoryService categoryService;
 
+//    @GetMapping("/categories")
+//    public List<Category> list() throws Exception {
+//        return categoryService.list();
+//    }
+
     @GetMapping("/categories")
-    public List<Category> list() throws Exception {
-        return categoryService.list();
+    public Page4Navigator<Category> list(@RequestParam(value = "start", defaultValue = "0") int start,
+                                         @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+        start = start<0?0:start;
+
+        //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        Page4Navigator<Category> page =categoryService.list(start, size, 5);
+
+        return page;
     }
 }
